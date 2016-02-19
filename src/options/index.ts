7eba7f5f52e7859d5a496f82ce74171dda0228e4
Anthony Liatsis <anthony@relateiq.com>
2016-02-18 16:53:51 -0800

    more options tests for class

--- src/options/index.ts
+++ src/options/index.ts
@@ -1,21 +1,22 @@
 import Trigger from '../trigger';
+import * as extend from 'extend';
 
-export interface Options {
-  trigger: Trigger;
-  content: string;
-  placement: string;
-  placementOffset: number;
-  optimizePlacement: boolean;
-  transitionPlacement: boolean;
-  alignment: string;
-  alignmentOffset: number;
-  viewportPadding: number;
-  timeToHoverOnPop: number;
-  showDelay: number;
-  fadeDuration: number;
+export interface IOptions {
+  trigger?: Trigger;
+  content?: string;
+  placement?: string;
+  placementOffset?: number;
+  optimizePlacement?: boolean;
+  transitionPlacement?: boolean;
+  alignment?: string;
+  alignmentOffset?: number;
+  viewportPadding?: number;
+  timeToHoverOnPop?: number;
+  showDelay?: number;
+  fadeDuration?: number;
 }
 
-export let defaultOptions: Options = {
+export let defaultOptions: IOptions = {
   trigger: new Trigger('hover'),
   content: '',
   placement: 'top',
@@ -29,3 +30,30 @@ export let defaultOptions: Options = {
   showDelay: 0,
   fadeDuration: 100
 };
+
+export class Options implements IOptions {
+  trigger: Trigger;
+  content: string;
+  placement: string;
+  placementOffset: number;
+  optimizePlacement: boolean;
+  transitionPlacement: boolean;
+  alignment: string;
+  alignmentOffset: number;
+  viewportPadding: number;
+  timeToHoverOnPop: number;
+  showDelay: number;
+  fadeDuration: number;
+
+  constructor(opts?: IOptions) {
+    this.extendDefault(opts);
+  }
+
+  extendDefault(opts: IOptions): Options {
+    return extend(this, defaultOptions, opts);
+  }
+
+  extend(opts: IOptions): Options {
+    return extend(this, opts);
+  }
+}
---
+++ src/options/options.spec.ts
@@ -0,0 +1,97 @@
+/// <reference path='../../typings/tsd.d.ts' />
+
+import {  } from './';
+
+import { Options, defaultOptions } from './';
+import Trigger from '../trigger';
+import * as extend from 'extend';
+
+let deepEqual = require('deep-equal');
+
+describe('Options - ', () => {
+
+  describe('default - ', () => {
+
+    it('should default trigger to a hover trigger', () => {
+      expect((new Options()).trigger).toEqual(new Trigger('hover'));
+    });
+
+    it('should default content to an empty string', () => {
+      expect((new Options()).content).toBe('');
+    });
+
+    it('should default placement to top', () => {
+      expect((new Options()).placement).toBe('top');
+    });
+
+    it('should default placementOffset to 8', () => {
+      expect((new Options()).placementOffset).toBe(8);
+    });
+
+    it('should default optimizePlacement to true', () => {
+      expect((new Options()).optimizePlacement).toBe(true);
+    });
+
+    it('should default transitionPlacement to true', () => {
+      expect((new Options()).transitionPlacement).toBe(true);
+    });
+
+    it('should default alignment to empty string', () => {
+      expect((new Options()).alignment).toBe('');
+    });
+
+    it('should default alignmentOffset to 0', () => {
+      expect((new Options()).alignmentOffset).toBe(0);
+    });
+
+    it('should default viewportPadding to 10', () => {
+      expect((new Options()).viewportPadding).toBe(10);
+    });
+
+    it('should default timeToHoverOnPop to 300', () => {
+      expect((new Options()).timeToHoverOnPop).toBe(300);
+    });
+
+    it('should default showDelay to 0', () => {
+      expect((new Options()).showDelay).toBe(0);
+    });
+
+    it('should default fadeDuration to 100', () => {
+      expect((new Options()).fadeDuration).toBe(100);
+    });
+
+  });
+
+  describe('extend - ', () => {
+
+    it('should extend defaults with custom options object passed to constructor', () => {
+      let expected = extend({}, defaultOptions); // copy
+      expected.trigger = new Trigger('click');
+      expected.content = 'hello world';
+      expected.placement = 'bottom';
+
+
+      expect(deepEqual((new Options({
+        trigger: expected.trigger,
+        content: expected.content,
+        placement: expected.placement
+      })), expected)).toBe(true);
+    });
+
+    it('should extend defaults with custom options object with extend fn', () => {
+      let expected = extend({}, defaultOptions); // copy
+      expected.trigger = new Trigger('click');
+      expected.content = 'hello world';
+      expected.placement = 'bottom';
+
+
+      expect(deepEqual((new Options().extend({
+        trigger: expected.trigger,
+        content: expected.content,
+        placement: expected.placement
+      })), expected)).toBe(true);
+    });
+
+  });
+
+});
