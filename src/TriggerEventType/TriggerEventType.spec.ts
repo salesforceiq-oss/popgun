361c95cb22499c2014b9bc34bb08508d0d203d47
Danyaal Rangwala <thedanyaal@gmail.com>
2016-03-10 16:41:59 -0800

    test triggerEventTypeToTriggerType

--- src/TriggerEventType/TriggerEventType.spec.ts
+++ src/TriggerEventType/TriggerEventType.spec.ts
@@ -4,20 +4,44 @@ import TriggerEventType from './';
 
 describe('TriggerEventType - ', () => {
 
-  it('should set click event type to `click`', () => {
-    expect(TriggerEventType.CLICK).toBe('click');
+  describe('TriggerEventTypes - ', () => {
+
+    it('should set click event type to `click`', () => {
+      expect(TriggerEventType.CLICK).toBe('click');
+    });
+
+    it('should set hover event type to `mouseenter`', () => {
+      expect(TriggerEventType.HOVER).toBe('mouseenter');
+    });
+
+    it('should set focus event type to `focusin`', () => {
+      expect(TriggerEventType.FOCUS).toBe('focusin');
+    });
+
+    it('should set manual event type to `popgun-manual`', () => {
+      expect(TriggerEventType.MANUAL).toBe('popgun-manual');
+    });
+
   });
 
-  it('should set hover event type to `mouseenter`', () => {
-    expect(TriggerEventType.HOVER).toBe('mouseenter');
-  });
+  describe('triggerEventTypeToTriggerType - ', () => {
 
-  it('should set focus event type to `focusin`', () => {
-    expect(TriggerEventType.FOCUS).toBe('focusin');
-  });
+    it('should return `click` for `click`', () => {
+      expect(TriggerEventType.triggerEventTypeToTriggerType('click')).toBe('click');
+    });
 
-  it('should set manual event type to `popgun-manual`', () => {
-    expect(TriggerEventType.MANUAL).toBe('popgun-manual');
+    it('should return `hover` for `mouseenter`', () => {
+      expect(TriggerEventType.triggerEventTypeToTriggerType('mouseenter')).toBe('hover');
+    });
+
+    it('should return `focus` for `focusin`', () => {
+      expect(TriggerEventType.triggerEventTypeToTriggerType('focusin')).toBe('focus');
+    });
+
+    it('should return `manual` for `popgun-manual`', () => {
+      expect(TriggerEventType.triggerEventTypeToTriggerType('popgun-manual')).toBe('manual');
+    });
+
   });
 
 });
