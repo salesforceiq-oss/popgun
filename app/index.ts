f1f1a8276d758cbd7bbb0034fb720af80028bc72
Danyaal Rangwala <thedanyaal@gmail.com>
2016-04-15 14:26:20 -0700

    update ui for demo

--- app/index.ts
+++ app/index.ts
@@ -5,6 +5,26 @@ let popgun = require('../.src').default;
 let el = document.createElement('div');
 el.setAttribute('popgun', '');
 el.setAttribute('id', 'inserted');
+el.setAttribute('popgun-trigger', 'click hover');
+el.setAttribute('popgun-html', 'HELLO WORLD!!');
 document.body.appendChild(el);
 
-popgun.hello();
+
+
+// Test Event Listeners
+
+el.addEventListener('PopgunContentSetup', function(e) {
+  console.log('PopgunContentSetup');
+}, false);
+
+el.addEventListener('PopgunPrePosition', function(e) {
+  console.log('PopgunPrePosition');
+}, false);
+
+el.addEventListener('PopgunPreShow', function(e) {
+  console.log('PopgunPreShow');
+}, false);
+
+el.addEventListener('PopgunShowing', function(e) {
+  console.log('PopgunShowing');
+}, false);
