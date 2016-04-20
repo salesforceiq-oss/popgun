b5e64960b7b0d8c37be65a5146ccfa0d4ae057e7
Danyaal Rangwala <thedanyaal@gmail.com>
2016-04-19 23:31:10 -0700

    add more than one pop for testing

--- app/index.ts
+++ app/index.ts
@@ -4,10 +4,32 @@ let popgun = require('../.src').default;
 
 let el = document.createElement('div');
 el.setAttribute('popgun', '');
+el.setAttribute('class', 'insert');
 el.setAttribute('id', 'inserted');
-el.setAttribute('popgun-trigger', 'click hover');
+el.setAttribute('popgun-group', 'test');
+el.setAttribute('popgun-trigger', 'click');
 el.setAttribute('popgun-html', 'HELLO WORLD!!');
 document.body.appendChild(el);
+
+let el2 = document.createElement('div');
+el2.setAttribute('popgun', '');
+el2.setAttribute('class', 'insert');
+el2.setAttribute('id', 'inserted2');
+el2.setAttribute('popgun-group', 'test');
+el2.setAttribute('popgun-trigger', 'hover');
+// el2.setAttribute('popgun-show-delay', '500');
+el2.setAttribute('popgun-html', 'BANANA!!');
+document.body.appendChild(el2);
+
+// let el3 = document.createElement('input');
+// el3.setAttribute('popgun', '');
+// // el3.setAttribute('class', 'insert');
+// // el3.setAttribute('id', 'inserted2');
+// el3.setAttribute('popgun-group', 'test');
+// el3.setAttribute('popgun-trigger', 'focus');
+// // el3.setAttribute('popgun-show-delay', '500');
+// el3.setAttribute('popgun-html', 'BANANA!!');
+// document.body.appendChild(el3);
 
 
 
