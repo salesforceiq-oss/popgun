cc6be7677968594bc69400d3a3971286d09f5339
Danyaal Rangwala <thedanyaal@gmail.com>
2016-05-31 22:49:34 -0700

    update ui for demo app

--- app/index.ts
+++ app/index.ts
@@ -2,46 +2,40 @@
 declare var require: NodeRequire;
 let popgun = require('../.src').default;
 
+let testSchema = { showDelay: 1000, trigger: 'hover' };
+let groupOpts = {
+  schema: 'testSchema',
+  options: { showDelay: 250, text: 'Wow! I cannot believe Popgun actually works!' }
+};
+popgun.addSchemaToSchemaStore('testSchema', testSchema);
+popgun.addGroupOptionsToGroupStore('test', groupOpts);
+
 let el = document.createElement('div');
 el.setAttribute('popgun', '');
-el.setAttribute('class', 'insert');
-el.setAttribute('id', 'inserted');
-el.setAttribute('popgun-group', 'nottest');
+el.setAttribute('class', 'target');
+el.setAttribute('popgun-group', 'test1');
 el.setAttribute('popgun-trigger', 'click');
-el.setAttribute('popgun-html', 'HELLO WORLD!!');
+el.setAttribute('popgun-html', '<div popgun popgun-group="test">Hover here for a nested pop!</div>');
+el.innerText = 'Click Me!';
 document.body.appendChild(el);
 
-let el2 = document.createElement('div');
-el2.setAttribute('popgun', '');
-el2.setAttribute('class', 'insert');
-el2.setAttribute('id', 'inserted2');
-el2.setAttribute('popgun-group', 'test');
-el2.setAttribute('popgun-trigger', 'hover click');
-// el2.setAttribute('popgun-show-delay', '500');
-el2.setAttribute('popgun-html', '<div popgun popgun-group="flomp" popgun-trigger="click" popgun-html="BANANA!!" style="width:50px; height:50px; background:yellow;"></div>');
-document.body.appendChild(el2);
+el = document.createElement('div');
+el.setAttribute('popgun', '');
+el.setAttribute('class', 'target different-class');
+el.setAttribute('popgun-group', 'test2');
+el.setAttribute('popgun-trigger', 'hover click');
+el.setAttribute('popgun-html', 'Why does being <i>up for something</i> and <i>down for something</i> mean the same thing?');
+el.innerText = 'Hover or Click Me!';
+document.body.appendChild(el);
 
-let el3 = document.createElement('div');
-el3.setAttribute('popgun', '');
-el3.setAttribute('class', 'insert');
-el3.setAttribute('id', 'inserted3');
-el3.setAttribute('popgun-group', 'nottest');
-el3.setAttribute('popgun-trigger', 'hover click');
-// el3.setAttribute('popgun-show-delay', '500');
-el3.setAttribute('popgun-html', '<img src="http://i.imgur.com/rTBj7U4.gif">');
-document.body.appendChild(el3);
-
-// let el3 = document.createElement('input');
-// el3.setAttribute('popgun', '');
-// // el3.setAttribute('class', 'insert');
-// // el3.setAttribute('id', 'inserted2');
-// el3.setAttribute('popgun-group', 'test');
-// el3.setAttribute('popgun-trigger', 'focus');
-// // el3.setAttribute('popgun-show-delay', '500');
-// el3.setAttribute('popgun-html', 'BANANA!!');
-// document.body.appendChild(el3);
-
-
+el = document.createElement('div');
+el.setAttribute('popgun', '');
+el.setAttribute('class', 'target');
+el.setAttribute('popgun-group', 'test1');
+el.setAttribute('popgun-trigger', 'hover');
+el.setAttribute('popgun-html', 'Pirates are way cooler than ninjas.');
+el.innerText = 'Hover Me!';
+document.body.appendChild(el);
 
 // Test Event Listeners
 
