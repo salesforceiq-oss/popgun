48f467867b0272e6c479501fbf7e75a81514745c
Danyaal Rangwala <thedanyaal@gmail.com>
2016-07-06 13:21:40 -0700

    address test case feedback

--- src/PopChainManager/PopChainManager.spec.ts
+++ src/PopChainManager/PopChainManager.spec.ts
@@ -35,6 +35,35 @@ describe('PopChainManager - ', () => {
 
     });
 
+
+    it('should remove pinned-pop attribute to elements in chain', () => {
+
+      let parentEl = document.createElement('div');
+      parentEl.setAttribute('popgun', '');
+      parentEl.setAttribute('popgun-group', 'parent');
+      parentEl.setAttribute('pinned-pop', '');
+
+      let childEl = document.createElement('div');
+      childEl.setAttribute('popgun', '');
+      childEl.setAttribute('popgun-group', 'child');
+      childEl.setAttribute('pinned-pop', '');
+
+      let t = new Trigger('click');
+
+      let parentPop = new Pop(parentEl, t);
+      let childPop = new Pop(childEl, t);
+
+      popEngine.addPopToPopStore('parent', parentPop);
+      popEngine.addPopToPopStore('child', childPop);
+
+      popChainManager.setParentChildRelationship(parentPop, childPop);
+      popChainManager.maybePinOrUnpinPopAndParentPops(childPop.targetEl, false);
+
+      expect(childEl.hasAttribute('pinned-pop')).toBe(false);
+      expect(parentEl.hasAttribute('pinned-pop')).toBe(false);
+
+    });
+
   });
 
   describe('setParentChildRelationship() - ', () => {
@@ -55,7 +84,10 @@ describe('PopChainManager - ', () => {
       popChainManager.setParentChildRelationship(parentPop, childPop);
 
       expect(parentPop.childPops[0]).toBe(childPop);
+      expect(parentPop.parentPop).toBe(null);
+
       expect(childPop.parentPop).toBe(parentPop);
+      expect(childPop.childPops.length).toBe(0);
 
     });
 
