578561dbb9566c14dbdcc4fccb3f57faa6940153
Danyaal Rangwala <thedanyaal@gmail.com>
2016-07-15 11:27:56 -0700

    move popchainmanager logic out of settimeout

--- src/PopEngine/index.ts
+++ src/PopEngine/index.ts
@@ -127,29 +127,32 @@ export class PopEngine {
 
     // this is gross and should be refactored
     // we store the old pop because it will be overwritten and we need it later
+    let container = <Element>document.querySelector('div[pop-id="' + groupId + '"]');
     let oldPop = this.getPopFromGroupId(groupId);
     this.addPopToPopStore(targetElement.getAttribute('popgun-group'), pop);
+    this._maybeSetParentChildRelationship(pop);
+    if (isAlreadyShowing && !!container) {
+      // if pop is already showing for group, reuse
+      if (!!oldPop && !!oldPop.childPops.length) {
+        oldPop.childPops.forEach(function(child: Pop): void {
+          this.hidePop(child.targetEl, false);
+        }, this);
+      }
+      if (!!oldPop && !!oldPop.parentPop) {
+        let idx = oldPop.parentPop.childPops.indexOf(oldPop);
+        if (idx !== -1) {
+          oldPop.parentPop.childPops.splice(idx, 1);
+        }
+      }
+    }
 
     // clear any timeouts and do a timeout and show pop
     this.clearTimeout(targetElement);
     this._timeouts.hoverdelay = setTimeout(function(): void {
       let animationEndStates = {};
-      let container = <Element>document.querySelector('div[pop-id="' + groupId + '"]');
-      this._maybeSetParentChildRelationship(pop);
 
       if (isAlreadyShowing && !!container) {
         // if pop is already showing for group, reuse
-        if (!!oldPop && !!oldPop.childPops.length) {
-          oldPop.childPops.forEach(function(child: Pop): void {
-            this.hidePop(child.targetEl, false);
-          }, this);
-        }
-        if (!!oldPop && !!oldPop.parentPop) {
-          let idx = oldPop.parentPop.childPops.indexOf(oldPop);
-          if (idx !== -1) {
-            oldPop.parentPop.childPops.splice(idx, 1);
-          }
-        }
         container.removeChild(container.getElementsByClassName('pop-content')[0]);
         this._maybeClearHandler(this._handlers[groupId]);
       } else {
