5a005352767bab9a959257dea47de0c6a26d1d45
Matthew Hui <matthewHuiCsc@gmail.com>
2016-08-05 01:06:27 -0700

    Made it so that user can choose how many popguns to generate

--- app/perf/perfApp.ts
+++ app/perf/perfApp.ts
@@ -2,11 +2,26 @@ declare var require: NodeRequire;
 import ExampleGenerator from './example-generator';
 (function() {
 	let exampleGenerator = new ExampleGenerator();
-
-	var examplesContainerEl = document.body.querySelector('#popgunPerfSection');
+	let examplesContainerEl:Element = document.body.querySelector('#popgunPerfSection');
 	if (examplesContainerEl) {
-		for (var idx = 0; idx < 1000; idx++) {
-			examplesContainerEl.appendChild(exampleGenerator.createPopgun());
-		}
+
+		let formEl:HTMLFormElement = <HTMLFormElement>document.body.querySelector('#perfGeneratorForm');
+		formEl.addEventListener('submit', function(event) {
+			event.preventDefault();
+			let numberInput:HTMLInputElement = <HTMLInputElement>formEl.querySelector('[name="numberInput"]');
+			let numOfPopguns:number = parseInt(numberInput.value);
+
+			if (typeof numOfPopguns !== 'undefined' && numOfPopguns >= 0) {
+				// could be optimized by just removing examplesContainerEl and readding back in
+				while(examplesContainerEl.firstChild) {
+					examplesContainerEl.removeChild(examplesContainerEl.firstChild);
+				}
+
+				// generate examples
+				for (var idx = 0; idx < numOfPopguns; idx++) {
+					examplesContainerEl.appendChild(exampleGenerator.createPopgun());
+				}
+			}
+		});
 	}
 })();
