e46cb438247b5d9b52f7e01e2b3d34290c40c30a
Matthew Hui <matthewHuiCsc@gmail.com>
2016-08-05 14:13:54 -0700

    Made changes according to comments on naming and unused styles

--- app/perf/example-generator.ts
+++ app/perf/example-generator.ts
@@ -8,20 +8,20 @@ export default class ExampleGenerator {
 	}
 
 	public createPopgun(): Element {
-		let trigger = document.createElement('div');
+		let target = document.createElement('div');
 
-		trigger.setAttribute('popgun', '');
-		trigger.setAttribute('pop-id', 'popgun-id-' + this.id);
+		target.setAttribute('popgun', '');
+		target.setAttribute('pop-id', 'popgun-id-' + this.id);
 		this.id++;
-		trigger.setAttribute('popgun-text', 'Hello World');
-		trigger.classList.add('popgun-example');
+		target.setAttribute('popgun-text', 'Hello World');
+		target.classList.add('popgun-example');
 
 		var block = document.createElement('div');
 		block.classList.add('pop-example-block');
 		block.setAttribute('style', 'background-color: ' + this.colorGenerator.generateColor());
 
-		trigger.appendChild(block);
-		return trigger;
+		target.appendChild(block);
+		return target;
 	}
 	id: number;
 	colorGenerator: ColorGenerator;
