2ca57522451ef4cf73904d885d59d00204dd0420
Danyaal Rangwala <thedanyaal@gmail.com>
2016-09-14 16:35:30 -0700

    minor refactoring + tips

--- app/exampleApp.ts
+++ app/exampleApp.ts
@@ -22,6 +22,7 @@ declare var require: NodeRequire;
     el1.setAttribute('popgun', '');
     setPopgunElements(el1);
     el1.setAttribute('class', 'target');
+    el1.setAttribute('popgun-html', '<h4 style="margin-top: 0;">Popgun Tip #1</h4><p>Popgun targets of the same group will be reused unless otherwise specified in the popgun options.</p>');
     el1.innerText = 'Target One';
     rightColumn.appendChild(el1);
 
@@ -29,6 +30,7 @@ declare var require: NodeRequire;
     el2.setAttribute('popgun', '');
     setPopgunElements(el2);
     el2.setAttribute('class', 'target different-class');
+    el2.setAttribute('popgun-html', '<h4 style="margin-top: 0;">Popgun Tip #2</h4><p>Clicking on a popgun target with a click trigger will pin the pop, so you can mouse out of the target and keep the popover open. Clicking out of the target or hitting the escape key will close the open popovers.</p>');
     el2.innerText = 'Target Two';
     rightColumn.appendChild(el2);
 
@@ -36,20 +38,33 @@ declare var require: NodeRequire;
     el3.setAttribute('popgun', '');
     setPopgunElements(el3);
     el3.setAttribute('class', 'target very-different-class');
+    el3.setAttribute('popgun-html', '<h4 style="margin-top: 0;">Popgun Tip #3</h4><p>Popgun will optimize the placement of the popover unless otherwise defined with the optimizePlacement, placement, and alignment options.</p>');
     el3.innerText = 'Target Three';
     rightColumn.appendChild(el3);
+
+    let el4 = document.createElement('input');
+    el4.setAttribute('popgun', '');
+    el4.setAttribute('popgun-trigger', 'focus');
+    el4.setAttribute('popgun-dark-style', 'true');
+    el4.setAttribute('popgun-html', '<h4 style="margin-top: 0;">Popgun Tip #4</h4><p>Popgun can display both text and rich HTML. If both are defined, Popgun will display the HTML.</p>');
+    el4.placeholder = 'Target Four';
+    rightColumn.appendChild(el4);
 
     let btn1 = document.createElement('button');
     btn1.innerText = 'Set Target One\'s Popgun Attributes';
     leftColumn.appendChild(btn1);
 
     let btn2 = document.createElement('button');
-    btn2.innerText = 'Set Target One\'s Popgun Attributes';
+    btn2.innerText = 'Set Target Two\'s Popgun Attributes';
     leftColumn.appendChild(btn2);
 
     let btn3 = document.createElement('button');
-    btn3.innerText = 'Set Target One\'s Popgun Attributes';
+    btn3.innerText = 'Set Target Three\'s Popgun Attributes';
     leftColumn.appendChild(btn3);
+
+    let btn4 = document.createElement('button');
+    btn4.innerText = 'Set Target Four\'s Popgun Attributes';
+    leftColumn.appendChild(btn4);
 
     btn1.addEventListener('click', function(e) {
       setPopgunElements(el1);
@@ -61,6 +76,10 @@ declare var require: NodeRequire;
 
     btn3.addEventListener('click', function(e) {
       setPopgunElements(el3);
+    });
+
+    btn4.addEventListener('click', function(e) {
+      setPopgunElements(el4);
     });
 
     function setPopgunElements(el: Element): void {
