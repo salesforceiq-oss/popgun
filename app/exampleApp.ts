3aaee9d321112a8df7b303876bc4fecaf0bd1496
Danyaal Rangwala <thedanyaal@gmail.com>
2016-09-13 19:43:45 -0700

    interactive demo app for popgun

--- app/exampleApp.ts
+++ app/exampleApp.ts
@@ -13,68 +13,127 @@ declare var require: NodeRequire;
 
 
   let exampleContainerEl = document.body.querySelector('#popgunExampleSection');
-  if (exampleContainerEl) {
-    let el = document.createElement('div');
-    el.setAttribute('popgun', '');
-    el.setAttribute('class', 'target');
-    el.setAttribute('popgun-group', 'test1');
-    el.setAttribute('popgun-trigger', 'click');
-    el.setAttribute('popgun-html', '<div popgun popgun-group="test" popgun-trigger="click hover">Click/Hover here for a nested pop!</div>');
-    el.innerText = 'Click Me!';
-    exampleContainerEl.appendChild(el);
+  let leftColumn = document.body.querySelector('#left');
+  let rightColumn = document.body.querySelector('#right');
 
-    el = document.createElement('div');
-    el.setAttribute('popgun', '');
-    el.setAttribute('class', 'target different-class');
-    el.setAttribute('popgun-group', 'test2');
-    el.setAttribute('popgun-trigger', 'hover click');
-    el.setAttribute('popgun-html', 'Why does being <i>up for something</i> and <i>down for something</i> mean the same thing?');
-    el.innerText = 'Hover or Click Me!';
-    exampleContainerEl.appendChild(el);
+  if (exampleContainerEl && leftColumn && rightColumn) {
+    // POPGUN TARGETS
+    let el1 = document.createElement('div');
+    el1.setAttribute('popgun', '');
+    setPopgunElements(el1);
+    el1.setAttribute('class', 'target');
+    el1.innerText = 'Target One';
+    rightColumn.appendChild(el1);
 
-    el = document.createElement('div');
-    el.setAttribute('popgun', '');
-    el.setAttribute('class', 'target');
-    el.setAttribute('popgun-group', 'test1');
-    el.setAttribute('popgun-trigger', 'hover');
-    el.setAttribute('popgun-dark-style', 'true');
-    el.setAttribute('popgun-html', 'Pirates are way cooler than ninjas.');
-    el.innerText = 'Hover Me I am dark styled!!';
-    exampleContainerEl.appendChild(el);
+    let el2 = document.createElement('div');
+    el2.setAttribute('popgun', '');
+    setPopgunElements(el2);
+    el2.setAttribute('class', 'target different-class');
+    el2.innerText = 'Target Two';
+    rightColumn.appendChild(el2);
 
-    let inp = document.createElement('input');
-    inp.setAttribute('type', 'text');
-    inp.setAttribute('placeholder', 'Focus me!');
-    inp.setAttribute('popgun', '');
-    inp.setAttribute('popgun-group', 'test3');
-    inp.setAttribute('popgun-trigger', 'focus');
-    inp.setAttribute('popgun-placement', 'right');
-    inp.setAttribute('popgun-html', '<div>Wow! Focusing too?!</div>');
-    exampleContainerEl.appendChild(inp);
+    let el3 = document.createElement('div');
+    el3.setAttribute('popgun', '');
+    setPopgunElements(el3);
+    el3.setAttribute('class', 'target very-different-class');
+    el3.innerText = 'Target Three';
+    rightColumn.appendChild(el3);
 
-    // Test Event Listeners
-    el.addEventListener('PopgunContentSetup', function(e) {
-      console.log('PopgunContentSetup');
-    }, false);
+    let btn1 = document.createElement('button');
+    btn1.innerText = 'Set Target One\'s Popgun Attributes';
+    leftColumn.appendChild(btn1);
 
-    el.addEventListener('PopgunPrePosition', function(e) {
-      console.log('PopgunPrePosition');
-    }, false);
+    let btn2 = document.createElement('button');
+    btn2.innerText = 'Set Target One\'s Popgun Attributes';
+    leftColumn.appendChild(btn2);
 
-    el.addEventListener('PopgunPreShow', function(e) {
-      console.log('PopgunPreShow');
-    }, false);
+    let btn3 = document.createElement('button');
+    btn3.innerText = 'Set Target One\'s Popgun Attributes';
+    leftColumn.appendChild(btn3);
 
-    el.addEventListener('PopgunShowing', function(e) {
-      console.log('PopgunShowing');
-    }, false);
+    btn1.addEventListener('click', function(e) {
+      setPopgunElements(el1);
+    });
 
-    el.addEventListener('PopgunPreHide', function(e) {
-      console.log('PopgunPreHide');
-    }, false);
+    btn2.addEventListener('click', function(e) {
+      setPopgunElements(el2);
+    });
 
-    el.addEventListener('PopgunHidden', function(e) {
-      console.log('PopgunHidden');
-    }, false);
+    btn3.addEventListener('click', function(e) {
+      setPopgunElements(el3);
+    });
+
+    function setPopgunElements(el: Element): void {
+      let trigger = (<HTMLInputElement>document.getElementById('popgun-trigger')).value;
+      el.setAttribute('popgun-trigger', trigger);
+
+      let group = (<HTMLInputElement>document.getElementById('popgun-group')).value;
+      el.setAttribute('popgun-group', group);
+
+      let placement = (<HTMLInputElement>document.getElementById('popgun-placement')).value;
+      el.setAttribute('popgun-placement', placement);
+
+      let alignment = (<HTMLInputElement>document.getElementById('popgun-alignment')).value;
+      el.setAttribute('popgun-alignment', alignment);
+
+      let timeToHoverOnPop = (<HTMLInputElement>document.getElementById('popgun-time-to-hover-on-pop')).value;
+      el.setAttribute('popgun-time-to-hover-on-pop', timeToHoverOnPop);
+
+      let showDelay = (<HTMLInputElement>document.getElementById('popgun-show-delay')).value;
+      el.setAttribute('popgun-show-delay', showDelay);
+
+      let tipClass = (<HTMLInputElement>document.getElementById('popgun-tip-class')).value;
+      el.setAttribute('popgun-tip-class', tipClass);
+
+      let text = (<HTMLInputElement>document.getElementById('popgun-text')).value;
+      el.setAttribute('popgun-text', text);
+
+      let html = (<HTMLInputElement>document.getElementById('popgun-html')).value;
+      el.setAttribute('popgun-html', html);
+
+      let darkStyle = (<HTMLInputElement>document.getElementById('popgun-dark-style')).checked;
+      el.setAttribute('popgun-dark-style', darkStyle.toString());
+
+      let optimizePlacement = (<HTMLInputElement>document.getElementById('popgun-optimize-placement')).checked;
+      el.setAttribute('popgun-optimize-placement', optimizePlacement.toString());
+
+      let disable = (<HTMLInputElement>document.getElementById('popgun-disable')).checked;
+      el.setAttribute('popgun-disable', disable.toString());
+
+      let reusePopover = (<HTMLInputElement>document.getElementById('popgun-reuse-popover')).checked;
+      el.setAttribute('popgun-reuse-popover', reusePopover.toString());
+
+      let eventListeners = (<HTMLInputElement>document.getElementById('popgun-event-listeners')).checked;
+      if (eventListeners) {
+        setEventListeners(el);
+      }
+    }
+
+    function setEventListeners(el: Element): void {
+      // Test Event Listeners
+      el.addEventListener('PopgunContentSetup', function(e) {
+        console.log('PopgunContentSetup fired!');
+      }, false);
+
+      el.addEventListener('PopgunPrePosition', function(e) {
+        console.log('PopgunPrePosition fired!');
+      }, false);
+
+      el.addEventListener('PopgunPreShow', function(e) {
+        console.log('PopgunPreShow fired!');
+      }, false);
+
+      el.addEventListener('PopgunShowing', function(e) {
+        console.log('PopgunShowing fired!');
+      }, false);
+
+      el.addEventListener('PopgunPreHide', function(e) {
+        console.log('PopgunPreHide fired!');
+      }, false);
+
+      el.addEventListener('PopgunHidden', function(e) {
+        console.log('PopgunHidden fired!');
+      }, false);
+    }
   }
 })();
